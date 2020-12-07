d57a5915f4d8e18d2c03265d8117f700357cba4a
Michael Stoll <michael.stoll@stolltec.com>
2020-12-07 11:27:08 +0100

    Vorlagen zips mit rungulp erstellen

--- 5.0/gulpfile.js
+++ 5.0/gulpfile.js
@@ -1,7 +1,11 @@
 
 var gulp = require("gulp"),
-cleanCSS = require("gulp-clean-css")
-sass = require('gulp-sass');
+cleanCSS = require("gulp-clean-css"),
+del = require("del"),
+git = require("gulp-git"),
+sass = require('gulp-sass'),
+zip = require("gulp-zip");
+
 sass.compiler = require('node-sass');
 
 
@@ -17,4 +21,27 @@ gulp.src(['styles/display.scss'])
 done();
 });
 
-gulp.task('default', gulp.parallel(['css']));
+
+gulp.task("zip", function(done) {
+    del(["../dist/import_*.zip",
+         "../dist/display_*.zip",
+         "../dist/printer_*.zip"], { 'force': true });
+    git.exec({
+        args: 'describe --tags',
+        quiet: true
+      }, (err, out) => {
+        var version = out.trim();
+        gulp.src("display/**")
+            .pipe(zip(`display_${version}.zip`))
+            .pipe(gulp.dest("../dist"));
+        gulp.src("import/**")
+            .pipe(zip(`import_${version}.zip`))
+            .pipe(gulp.dest("../dist"));
+        gulp.src("printer/**")
+            .pipe(zip(`printer_${version}.zip`))
+            .pipe(gulp.dest("../dist"));
+    });
+    done();
+});
+
+gulp.task('default', gulp.series(['css', 'zip']));
