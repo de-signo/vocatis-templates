989b067a53310e322bc93b31eb2485b178eb5009
Michael Stoll <michael.stoll@stolltec.com>
2021-01-29 10:18:13 +0100

    Sprachausgabe sequentiell abspielen

--- 5.0/display/site.js
+++ 5.0/display/site.js
@@ -1,8 +1,30 @@
 
 (function(sitejs, $) {
+
+    function setNextTrack() {
+        var audio = sitejs._audio;
+        if (audio.children.length > 0)
+        {
+            audio.load();
+            audio.play();
+        }
+    }
+
     sitejs.initVocatis = function(refreshInterval)
     {
         var popupContent = $("#popup").html();
+        var audio = sitejs._audio = document.getElementById("notify");
+
+        if (audio) {
+            audio.addEventListener("ended", function(e) {
+                audio.removeChild(audio.children[0]);
+                setNextTrack();
+            }, false);
+
+            audio.addEventListener("error", function(e) {
+                console.error("Audio has error.");
+            }, false);
+        }
 
         $("#list")
             .vocatisList({
@@ -11,10 +33,20 @@
             .highlightNumbers({
                 items: "tr:not(.spacer)",
                 call: function (items) {
-                    var audio = document.getElementById("notify");
                     if (audio) {
-                        audio.src = $(audio).data("src").replace("~number~", items[0].number).replace("~room~", items[0].room);
-                        audio.play();
+                        items.forEach(function(item) {
+                            var source = document.createElement("source");
+                            var atSrc = document.createAttribute("src");
+                            atSrc.value = $(audio).data("src").replace("~number~", item.number).replace("~room~", item.room);
+                            source.attributes.setNamedItem(atSrc);
+                            var atType = document.createAttribute("type");
+                            atType.value = "audio/mp3";
+                            source.attributes.setNamedItem(atType);
+                            audio.appendChild(source);
+                        });
+                        if (audio.paused) {
+                            setNextTrack();
+                        }
                     }
                     if (popupContent) {
                         $("#popup").html(popupContent.replace("~number~", items[0].number).replace("~room~", items[0].room)).show();
