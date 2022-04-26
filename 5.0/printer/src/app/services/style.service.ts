b5e11f63012ce1f50fd539975b5cba7d59dbe797
Michael Stoll <michael.stoll@stolltec.com>
2022-04-26 11:25:08 +0200

    printer: appointment Feld-Bezeichnungen abgestimmt

--- 5.0/printer/src/app/services/style.service.ts
+++ 5.0/printer/src/app/services/style.service.ts
@@ -45,8 +45,8 @@ export class StyleService {
         let i = 1;
         let p2q = {} as {[key: string]: {queue: string, categories: string[]}};
         while (true) {
-          let pn = params["s/plan_name" + i];
-          let qi = params["s/queueid" + i];
+          let pn = params["s/ap_pn" + i];
+          let qi = params["s/ap_qi" + i];
           if (!pn && !qi)
             break;
           p2q[pn] = { queue: qi, categories: apt_cats };
