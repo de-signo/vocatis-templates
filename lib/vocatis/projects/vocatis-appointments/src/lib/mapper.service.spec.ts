1681c24a0988d77b13c9b67a1c3d2609877f8968
Michael Stoll <michael.stoll@stolltec.com>
2023-06-27 09:53:35 +0200

    Add license headers

--- lib/vocatis/projects/vocatis-appointments/src/lib/mapper.service.spec.ts
+++ lib/vocatis/projects/vocatis-appointments/src/lib/mapper.service.spec.ts
@@ -1,8 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+/*
+ *  Copyright (C) 2023 DE SIGNO GmbH
+ *
+ *  This program is dual licensed. If you did not license the program under
+ *  differnt tems, the following applies:
+ *
+ *  This program is free software: You can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
-import { MapperService } from './mapper.service';
+import { TestBed } from "@angular/core/testing";
 
-describe('MapperService', () => {
+import { MapperService } from "./mapper.service";
+
+describe("MapperService", () => {
   let service: MapperService;
 
   beforeEach(() => {
@@ -10,7 +31,7 @@ describe('MapperService', () => {
     service = TestBed.inject(MapperService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
