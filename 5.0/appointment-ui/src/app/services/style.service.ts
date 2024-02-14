7a124e23bd10e072131911576cb6377a1267c885
Michael Stoll <michael.stoll@stolltec.com>
2024-02-14 11:58:25 +0100

    appointment-ui: Migrate StyleService to use TempateService

--- 5.0/appointment-ui/src/app/services/style.service.ts
+++ 5.0/appointment-ui/src/app/services/style.service.ts
@@ -20,8 +20,8 @@
  */
 
 import { Injectable } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
 import { IAppointmentOptions } from "vocatis-lib/dist/vocatis-appointments";
+import { TemplateService } from "@isign/forms-templates";
 
 @Injectable({
   providedIn: "root",
@@ -31,27 +31,28 @@ export class StyleService implements IAppointmentOptions {
   planToQueue: { [key: string]: { queue: string; categories: string[] } } = {};
   numberSource?: "auto" | "T3" | "T4" | "T5" | "user";
 
-  constructor(route: ActivatedRoute) {
-    route.queryParams.subscribe((params) => {
-      const pp = params["s/pp"];
-      this.postponeOffset =
-        !pp && pp != "0" && pp != 0 ? undefined : 1000 * parseInt(pp);
+  constructor(tmplSvc: TemplateService) {
+    const tmpl = tmplSvc.getTemplate();
+    const pp = tmpl.parameters["pp"];
+    this.postponeOffset =
+      !pp && pp != "0" && <any>pp != 0
+        ? undefined
+        : 1000 * parseInt(pp ?? "NaN");
 
-      // read plan / queue map
-      const apt_cats = params["s/ap_cat"] ?? [];
-      let i = 1;
-      let p2q = {} as {
-        [key: string]: { queue: string; categories: string[] };
-      };
-      while (true) {
-        let pn = params["s/ap_pn" + i];
-        let qi = params["s/ap_qi" + i];
-        if (!pn && !qi) break;
-        p2q[pn] = { queue: qi, categories: apt_cats };
-        i++;
-      }
-      this.planToQueue = p2q;
-      this.numberSource = params["s/ns"] ?? "auto";
-    });
+    // read plan / queue map
+    const apt_cats = (tmpl.parameters["ap_cat"] ?? "").split(",");
+    let i = 1;
+    let p2q = {} as {
+      [key: string]: { queue: string; categories: string[] };
+    };
+    while (true) {
+      let pn = tmpl.parameters["ap_pn" + i];
+      let qi = tmpl.parameters["ap_qi" + i];
+      if (!pn || !qi) break;
+      p2q[pn] = { queue: qi, categories: apt_cats };
+      i++;
+    }
+    this.planToQueue = p2q;
+    this.numberSource = <any>tmpl.parameters["ns"] ?? "auto";
   }
 }
