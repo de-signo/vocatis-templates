fa6c19f27b00442a93c96b8533300b838038c40a
Michael Stoll <info@de-signo.de>
2024-08-30 12:02:14 +0200

    printer: Make app environment non templated

--- 5.0/printer/app_src/environments/environment.prod.ts
+++ 5.0/printer/app_src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  refreshInterval: 10 * 1000, /* ms */
+  refreshInterval: 10 * 1000 /* ms */,
   // use undefined for default (in development mode the demo data files will be used)
-  wellKnownISignUrl: undefined
+  wellKnownISignUrl: undefined,
 };
